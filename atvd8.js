e82fe90b387dd9eef5e49ee0ea340bf39c0a2908
Luis Santos <luisguerreiro095@gmail.com>
2023-10-12 19:54:57 -0300

    Atualizando código

--- atvd8.js
+++ atvd8.js
@@ -10,18 +10,8 @@ this.peso = peso
 this.preco = preco
 }
  
-function fruta(nome, tipo, cor, peso, preco){
-    this.comprador = pessoa 
-    this.nome = nome 
-    this.tipo = tipo
-    this.cor = cor
-    this.peso = peso
-    this.preco = preco
-    }
-     
-
 const fruta1 = new fruta("Banana","Doce", "Amarelo", 75 + " gramas", "R$" + 2.50)
-const fruta2 = new fruta("Maracujá", "Semiácida", "Amarelo", 60 + "gramas", "R$" + 3.50)
+const fruta2 = new fruta("Maracujá", "Semiácida", "Amarelo", 60 + " gramas", "R$" + 3.50)
 
 console.log(fruta1)
 console.log(fruta2)
