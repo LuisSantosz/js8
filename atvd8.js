1ea8c152c9ca189115fb11ad600ff01924a4efe2
Luis Santos <luisguerreiro095@gmail.com>
2023-10-12 19:53:12 -0300

    Upload da ativadade 8

---
+++ atvd8.js
@@ -0,0 +1,31 @@
+pessoa = prompt("Informe seu nome: ")
+
+
+function fruta(nome, tipo, cor, peso, preco){
+this.comprador = pessoa 
+this.nome = nome 
+this.tipo = tipo
+this.cor = cor
+this.peso = peso
+this.preco = preco
+}
+ 
+function fruta(nome, tipo, cor, peso, preco){
+    this.comprador = pessoa 
+    this.nome = nome 
+    this.tipo = tipo
+    this.cor = cor
+    this.peso = peso
+    this.preco = preco
+    }
+     
+
+const fruta1 = new fruta("Banana","Doce", "Amarelo", 75 + " gramas", "R$" + 2.50)
+const fruta2 = new fruta("Maracujá", "Semiácida", "Amarelo", 60 + "gramas", "R$" + 3.50)
+
+console.log(fruta1)
+console.log(fruta2)
+
+
+
+
